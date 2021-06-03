2319ae74c8b12fef7e5bd446a9aa1ad321522d30
unryeong <leyng7@gmail.com>
2021-06-03 22:27:33 +0900

    first commit

---
+++ src/main/java/me/ryeong/ggjm/GgjmApplication.java
@@ -0,0 +1,13 @@
+package me.ryeong.ggjm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgjmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GgjmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/ryeong/ggjm/GgjmApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ryeong.ggjm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GgjmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
